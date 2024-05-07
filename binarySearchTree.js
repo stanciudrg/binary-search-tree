cc843196a90cbfede2f444184b8f7bdd0d7efff4
stanciudrg <stanciudragos1337@gmail.com>
2024-05-07 16:53:59 +0300

    Create Tree class with cleanArray and root properties

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -6,3 +6,14 @@ class Node {
     this.right = null;
   }
 }
+
+class Tree {
+  constructor(array) {
+    // Holds the array sorted with the mergeSort algorithm
+    this.cleanArray = array ? mergeSort([...new Set(array)]) : null;
+    // Holds the tree built from the sorted array
+    this.root = this.cleanArray
+      ? this.buildTree(this.cleanArray, 0, this.cleanArray.length - 1)
+      : null;
+  }
+}
