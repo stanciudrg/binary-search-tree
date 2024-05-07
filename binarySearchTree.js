a6f7d7030741a8d82f053e28a5a18a3edb89e8ae
stanciudrg <stanciudragos1337@gmail.com>
2024-05-07 17:04:38 +0300

    Turn several methods private

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -13,47 +13,47 @@ class Tree {
     this.cleanArray = array ? mergeSort([...new Set(array)]) : null;
     // Holds the tree built from the sorted array
     this.root = this.cleanArray
-      ? this.buildTree(this.cleanArray, 0, this.cleanArray.length - 1)
+      ? this.#buildTree(this.cleanArray, 0, this.cleanArray.length - 1)
       : null;
   }
 
   // Turns an array into a balanced binary tree full of Node objects appropriately placed
   // and returns the level-0 root node
-  buildTree(array, start, end) {
+  #buildTree(array, start, end) {
     if (start > end) return null;
 
     const mid = parseInt((start + end) / 2);
     const node = new Node(array[mid]);
-    node.left = this.buildTree(array, start, mid - 1);
-    node.right = this.buildTree(array, mid + 1, end);
+    node.left = this.#buildTree(array, start, mid - 1);
+    node.right = this.#buildTree(array, mid + 1, end);
 
     return node;
   }
 
   insert(value) {
-    this.insertNode(this.root, value);
+    this.#insertNode(this.root, value);
   }
 
   // Inserts a given value into the tree
-  insertNode(root, value) {
+  #insertNode(root, value) {
     if (root === null) return (root = new Node(value));
-    if (root.data > value) root.left = this.insertNode(root.left, value);
-    if (root.data < value) root.right = this.insertNode(root.right, value);
+    if (root.data > value) root.left = this.#insertNode(root.left, value);
+    if (root.data < value) root.right = this.#insertNode(root.right, value);
 
     return root;
   }
 
   delete(value) {
-    this.deleteNode(this.root, value);
+    this.#deleteNode(this.root, value);
   }
 
   // Deletes a given value from the tree
-  deleteNode(root, value) {
+  #deleteNode(root, value) {
     if (root === null) return root;
     // If value is bigger than current root's data, the node must be on the left subtree
-    if (root.data > value) root.left = this.deleteNode(root.left, value);
+    if (root.data > value) root.left = this.#deleteNode(root.left, value);
     // If value is lower than current root's data, the node must be on the right subtree
-    if (root.data < value) root.right = this.deleteNode(root.right, value);
+    if (root.data < value) root.right = this.#deleteNode(root.right, value);
 
     // If the node is found,
     if (root.data === value) {
@@ -62,27 +62,27 @@ class Tree {
       if (root.right === null) return root.left;
 
       // Otherwise find the closest successor of this node
-      const closestSuccessor = this.getLowestNode(root.right);
+      const closestSuccessor = this.#getLowestNode(root.right);
       // Replace the data of this node with the data of its closest successor...
       root.data = closestSuccessor.data;
       // ...and delete the closest successor (value == root.data == closestSuccessor.data)
-      root.right = this.deleteNode(root.right, root.data);
+      root.right = this.#deleteNode(root.right, root.data);
     }
 
     return root;
   }
 
   find(value) {
-    return this.findNode(this.root, value);
+    return this.#findNode(this.root, value);
   }
 
   // Returns the node with the given value, if found
-  findNode(root, value) {
+  #findNode(root, value) {
     if (root === null) return;
     if (root.data === value) return root;
 
-    if (root.data > value) return this.findNode(root.left, value);
-    if (root.data < value) return this.findNode(root.right, value);
+    if (root.data > value) return this.#findNode(root.left, value);
+    if (root.data < value) return this.#findNode(root.right, value);
 
     return root;
   }
@@ -91,19 +91,19 @@ class Tree {
     if (!node) return;
     if (!node.data) return;
 
-    return this.getHeight(node, 0);
+    return this.#getHeight(node, 0);
   }
 
   // Returns the height of the node, which is defined as the number of edges
   // in the longest path from a given node to a leaf node.
-  getHeight(root, level) {
+  #getHeight(root, level) {
     if (root === null) return level;
 
     // Increase the level with each recursion to compare which
     // path reached a higher level
     const newLevel = level + 1;
-    const leftLevel = this.getHeight(root.left, newLevel);
-    const rightLevel = this.getHeight(root.right, newLevel);
+    const leftLevel = this.#getHeight(root.left, newLevel);
+    const rightLevel = this.#getHeight(root.right, newLevel);
 
     if (leftLevel > rightLevel) {
       return leftLevel;
@@ -116,12 +116,12 @@ class Tree {
     if (!node) return;
     if (!node.data) return;
 
-    return this.getDepth(this.root, node, 0);
+    return this.#getDepth(this.root, node, 0);
   }
 
   // Returns the depth of the node, which is defined as the number of edges in
   // the path from a given node to the tree’s root node.
-  getDepth(root, node, level) {
+  #getDepth(root, node, level) {
     if (root === null) return;
     // Use 1-based indexing
     if (root.data == node.data) return level + 1;
@@ -129,8 +129,8 @@ class Tree {
     // Increase the level with each recursion to determine how many
     // levels it took to reach the root starting from the node
     const newLevel = level + 1;
-    const leftLevel = this.getDepth(root.left, node, newLevel);
-    const rightLevel = this.getDepth(root.right, node, newLevel);
+    const leftLevel = this.#getDepth(root.left, node, newLevel);
+    const rightLevel = this.#getDepth(root.right, node, newLevel);
 
     if (leftLevel) {
       return leftLevel;
@@ -140,11 +140,11 @@ class Tree {
   }
 
   levelOrder(callback) {
-    return this.levelOrderIterative(callback);
+    return this.#levelOrderIterative(callback);
   }
 
   // Breadth-first level order traversal
-  levelOrderIterative(callback) {
+  #levelOrderIterative(callback) {
     if (this.root === null) return;
 
     // Queue used to keep track of the nodes on each level
@@ -253,22 +253,22 @@ class Tree {
   }
 
   isBalanced() {
-    return this.isBalancedIterative(this.root);
+    return this.#isBalancedRecursive(this.root);
   }
 
   // Checks if the difference between the heights of the left subtree and
   // the heights of the right subtree of every node is not more than 1
-  isBalancedRecursive(root) {
+  #isBalancedRecursive(root) {
     if (root === null) return;
 
     // Get and compare the heights of the existing subtrees
-    const leftHeight = root.left ? this.getHeight(root.left, 0) : null;
-    const rightHeight = root.right ? this.getHeight(root.right, 0) : null;
+    const leftHeight = root.left ? this.#getHeight(root.left, 0) : null;
+    const rightHeight = root.right ? this.#getHeight(root.right, 0) : null;
     // If the difference between the heights is bigger than 2, then the tree is not balanced
     if (Math.abs(leftHeight - rightHeight) > 1) return false;
 
-    const left = this.isBalancedRecursive(root.left);
-    const right = this.isBalancedRecursive(root.right);
+    const left = this.#isBalancedRecursive(root.left);
+    const right = this.#isBalancedRecursive(root.right);
     // If either one of the subtrees returned false, then the tree is not balanced
     if (left === false || right === false) return false;
     return true;
@@ -281,13 +281,13 @@ class Tree {
     // ! inOrder() returns an array of values if no callback is passed
     const newArray = this.inOrder();
     // Uses the array of sorted values to build a new balanced tree
-    // ! buildTree will always build a balanced tree as long as the provided
+    // ! #buildTree() will always build a balanced tree as long as the provided
     // array is sorted.
-    this.root = this.buildTree(newArray, 0, newArray.length - 1);
+    this.root = this.#buildTree(newArray, 0, newArray.length - 1);
   }
 
   // Finds and returns the lowest node inside the provided tree
-  getLowestNode(root) {
+  #getLowestNode(root) {
     let temp = root;
     let lowest = temp;
 
