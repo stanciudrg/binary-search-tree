6b0857b3b2418da4f3f573db8909e79617f0a41c
stanciudrg <stanciudragos1337@gmail.com>
2024-05-07 16:55:02 +0300

    Create delete and deleteNode methods

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -42,4 +42,33 @@ class Tree {
 
     return root;
   }
+
+  delete(value) {
+    this.deleteNode(this.root, value);
+  }
+
+  // Deletes a given value from the tree
+  deleteNode(root, value) {
+    if (root === null) return root;
+    // If value is bigger than current root's data, the node must be on the left subtree
+    if (root.data > value) root.left = this.deleteNode(root.left, value);
+    // If value is lower than current root's data, the node must be on the right subtree
+    if (root.data < value) root.right = this.deleteNode(root.right, value);
+
+    // If the node is found,
+    if (root.data === value) {
+      // And it has only one child,  return that child
+      if (root.left === null) return root.right;
+      if (root.right === null) return root.left;
+
+      // Otherwise find the closest successor of this node
+      const closestSuccessor = this.getLowestNode(root.right);
+      // Replace the data of this node with the data of its closest successor...
+      root.data = closestSuccessor.data;
+      // ...and delete the closest successor (value == root.data == closestSuccessor.data)
+      root.right = this.deleteNode(root.right, root.data);
+    }
+
+    return root;
+  }
 }
